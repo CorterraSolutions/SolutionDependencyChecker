7fda4bf47e9ecd65669d501d0e78cfa5bf4624e4
jbonomo <jbonomo@hbsys.com>
2019-02-02 12:17:33 -0600

    Changed ID

--- XRMSolutionDependencyChecker/Properties/AssemblyInfo.cs
+++ XRMSolutionDependencyChecker/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("80f13f9f-521e-4ebb-b547-fc88f9952f81")]
+[assembly: Guid("50f13f9f-521e-4ebb-b547-fc88f4952f81")]
 
 // Version information for an assembly consists of the following four values:
 //
