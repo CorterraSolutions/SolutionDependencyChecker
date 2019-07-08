433d4bb7889869c6405e151eda05e22e848df066
MiddyE57 <MiddyE57@gmail.com>
2019-07-08 16:21:54 -0500

    20190708 DM - Dependency Checker Updates

---
+++ PluginControl.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using McTools.Xrm.Connection;
+using System.IO;
+using Microsoft.Crm.Sdk.Messages;
+using System.Runtime.InteropServices;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace XRMSolutionDependencyChecker
+{
+    public partial class PluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+        private IOrganizationService sourceEnviornment;
+        private Dictionary<int, Entity> sourceSolutions = new Dictionary<int, Entity>();
+        private Dictionary<int, string> TypeIcon_Dictionary =
+            new Dictionary<int, string>()
+            {
+                { 1, "Entity" },
+                { 2, "Field" },
+                { 3, "Relationship" },
+                { 4, "Attribute Picklist Value" },
+                { 5, "Attribute Lookup Value" },
+                { 6, "View Attribute" },
+                { 7, "Localized Label" },
+                { 8, "Relationship Extra Condition" },
+                { 9, "Option Set" },
+                { 10, "Entity Relationship" },
+                { 11, "Entity Relationship Role" },
+                { 12, "Entity Relationship Relationships" },
+                { 13, "Managed Property" },
+                { 14, "Entity Key" },
+                { 16, "Privilege" },
+                { 17, "PrivilegeObjectTypeCode" },
+                { 18, "Index" },
+                { 20, "Role" },
+                { 21, "Role Privilege" },
+                { 22, "Display String" },
+                { 23, "Display String Map" },
+                { 24, "Form" },
+                { 25, "Organization" },
+                { 26, "View" },
+                { 29, "Dialog/Workflow" },
+                { 31, "Report" },
+                { 32, "Report Entity" },
+                { 33, "Report Category" },
+                { 34, "Report Visibility" },
+                { 35, "Attachment" },
+                { 36, "Email Template" },
+                { 37, "Contract Template" },
+                { 38, "KB Article Template" },
+                { 39, "Mail Merge Template" },
+                { 44, "Duplicate Rule" },
+                { 45, "Duplicate Rule Condition" },
+                { 46, "Entity Map" },
+                { 47, "Attribute Map" },
+                { 48, "Ribbon Command" },
+                { 49, "Ribbon Context Group" },
+                { 50, "Ribbon Customization" },
+                { 52, "Ribbon Rule" },
+                { 53, "Ribbon Tab To Command Map" },
+                { 55, "Ribbon Diff" },
+                { 59, "Chart" },
+                { 60, "View" },
+                { 61, "Web Resource" },
+                { 62, "Site Map" },
+                { 63, "Connection Role" },
+                { 64, "Complex Control" },
+                { 65, "Hierachy Rule" },
+                { 66, "Custom Control" },
+                { 68, "Custom Control Default Config" },
+                { 70, "Field Security Profile" },
+                { 71, "Field Permission" },
+                { 90, "Plugin Type"},
+                { 91, "Plugin Assembly" },
+                { 92, "SDK Message Processing Step" },
+                { 93, "SDK Message Processing Step Image" },
+                { 95, "Service Endpoint" },
+
+            };
+
+        public PluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            //ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+
+            txt_OutputPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void LoadSolution_Button_Click(object sender, EventArgs e)
+        {
+            OpenSolution.ShowDialog();
+
+            if (OpenSolution.SafeFileName == "OpenSolution")
+                return;
+
+            //if (ProgressPanel.Visible != true)
+            //    this.Height = this.Height + ProgressPanel.Height;
+
+            //ProgressPanel.Visible = true;
+
+            //Progress_Label.Text = $"Opening...{OpenSolution.FileName + Environment.NewLine}";
+            //Progress_Label.Refresh();
+
+            byte[] SolutionFile = File.ReadAllBytes(OpenSolution.FileName);
+
+            BackgroundProgressIndicator.RunWorkerAsync(SolutionFile);
+
+            string Status = ShowMissingComponents(SolutionFile);
+
+            if (Status == "Succeeded")
+                output_txt.Text = $@"Found missing dependencies in target enviornment... {Environment.NewLine}{OpenSolution.FileName}. {Environment.NewLine}Output available at: {txt_OutputPath.Text}\dependencies.csv";
+            else if (Status == "No Missing Components")
+                output_txt.Text = $"There were no missing components in {Environment.NewLine + OpenSolution.SafeFileName}";
+            else
+                output_txt.Text = Status;
+        }
+
+        public string ShowMissingComponents(byte[] ExportedSolution)
+        {
+            try
+            {
+                SolutionComponents_ListView.Items.Clear();
+
+                RetrieveMissingComponentsRequest GetMissingComponents_Request = new RetrieveMissingComponentsRequest
+                {
+                    CustomizationFile = ExportedSolution
+                };
+
+                RetrieveMissingComponentsResponse GetMissingComponents = (RetrieveMissingComponentsResponse)base.Service.Execute(GetMissingComponents_Request);
+
+                if (GetMissingComponents.MissingComponents.Count() == 0)
+                {
+                    panel1.Visible = false;
+                    return "No Missing Components";
+                }
+                    
+                // Create csv string builder
+                var csv = new StringBuilder();
+
+                // Set headers on file
+                csv.AppendLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}",
+                    "Dependent Component Type",
+                    "Dependent Component Display Name",
+                    "Dependent Component Schema Name",
+                    "Dependent Component ID",
+                    "Required Component Type",
+                    "Required Component Display Name",
+                    "Required Component Schema Name",
+                    "Required Component ID"
+                ));
+
+                // loop
+                foreach (MissingComponent MissingComponent in GetMissingComponents.MissingComponents)
+                {
+                    ListViewItem MissingComponent_ListViewItem = new ListViewItem(
+                        (string.IsNullOrEmpty(MissingComponent.RequiredComponent.ParentDisplayName) ? "" : MissingComponent.RequiredComponent.ParentDisplayName + " (" + MissingComponent.RequiredComponent.ParentSchemaName + ") | ")
+                         + (TypeIcon_Dictionary.ContainsKey(MissingComponent.RequiredComponent.Type) ? TypeIcon_Dictionary[MissingComponent.RequiredComponent.Type] : "Type Code: " + MissingComponent.RequiredComponent.Type.ToString())
+                        + " | " + MissingComponent.RequiredComponent.DisplayName + " | " + MissingComponent.RequiredComponent.SchemaName
+                    );
+
+                    SolutionComponents_ListView.Items.Add(MissingComponent_ListViewItem);
+
+                    csv.AppendLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}",
+                        $"{(TypeIcon_Dictionary.ContainsKey(MissingComponent.DependentComponent.Type) ? TypeIcon_Dictionary[MissingComponent.DependentComponent.Type] : "Type Code: " + MissingComponent.DependentComponent.Type.ToString())}",
+                        $"{MissingComponent.DependentComponent.DisplayName}",
+                        $"{MissingComponent.DependentComponent.SchemaName}",
+                        $"{MissingComponent.DependentComponent.Id}",
+                        $"{(TypeIcon_Dictionary.ContainsKey(MissingComponent.RequiredComponent.Type) ? TypeIcon_Dictionary[MissingComponent.RequiredComponent.Type] : "Type Code: " + MissingComponent.RequiredComponent.Type.ToString())}",
+                        $"{MissingComponent.RequiredComponent.DisplayName}",
+                        $"{MissingComponent.RequiredComponent.SchemaName}",
+                        $"{MissingComponent.RequiredComponent.Id}"
+                    ));
+                }
+
+                // write csv file
+                File.WriteAllText($@"{txt_OutputPath.Text}\dependencies.csv", csv.ToString());
+
+                if (panel1.Visible != true)
+                    this.Height = this.Height + panel1.Height;
+
+                panel1.Visible = true;
+
+                return "Succeeded";
+            }
+            catch (Exception error)
+            {
+                return error.ToString();
+            }
+        }
+
+        public string GetTypeIcon(int type)
+        {
+            if (TypeIcon_Dictionary.ContainsKey(type))
+                return TypeIcon_Dictionary[type];
+            else
+                return "default";
+        }
+
+        private void SolutionComponents_ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenSolution_Computer_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PluginControl.designer.cs
@@ -0,0 +1,237 @@
+﻿namespace XRMSolutionDependencyChecker
+{
+    partial class PluginControl 
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PluginControl));
+            this.BackgroundProgressIndicator = new System.ComponentModel.BackgroundWorker();
+            this.InfoLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.ComonentIcons = new System.Windows.Forms.ImageList(this.components);
+            this.SolutionComponents_ListView = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OpenSolution_Computer = new System.Windows.Forms.GroupBox();
+            this.LoadSolution_Button = new System.Windows.Forms.Button();
+            this.OpenSolution = new System.Windows.Forms.OpenFileDialog();
+            this.txt_OutputPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output_txt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.OpenSolution_Computer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(417, 32);
+            this.InfoLabel.Text = "Missing Components in enviornment:";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.InfoLabel});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Padding = new System.Windows.Forms.Padding(0);
+            this.toolStrip2.Size = new System.Drawing.Size(1940, 35);
+            this.toolStrip2.TabIndex = 0;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // ComonentIcons
+            // 
+            this.ComonentIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ComonentIcons.ImageStream")));
+            this.ComonentIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.ComonentIcons.Images.SetKeyName(0, "Entity");
+            this.ComonentIcons.Images.SetKeyName(1, "BussinessRule");
+            this.ComonentIcons.Images.SetKeyName(2, "SLA");
+            this.ComonentIcons.Images.SetKeyName(3, "PlugIn");
+            this.ComonentIcons.Images.SetKeyName(4, "Process");
+            this.ComonentIcons.Images.SetKeyName(5, "Workflow");
+            this.ComonentIcons.Images.SetKeyName(6, "default");
+            this.ComonentIcons.Images.SetKeyName(7, "LoadingIcon");
+            // 
+            // SolutionComponents_ListView
+            // 
+            this.SolutionComponents_ListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SolutionComponents_ListView.FullRowSelect = true;
+            this.SolutionComponents_ListView.GridLines = true;
+            this.SolutionComponents_ListView.LabelWrap = false;
+            this.SolutionComponents_ListView.LargeImageList = this.ComonentIcons;
+            this.SolutionComponents_ListView.Location = new System.Drawing.Point(8, 44);
+            this.SolutionComponents_ListView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SolutionComponents_ListView.Name = "SolutionComponents_ListView";
+            this.SolutionComponents_ListView.Size = new System.Drawing.Size(1916, 550);
+            this.SolutionComponents_ListView.SmallImageList = this.ComonentIcons;
+            this.SolutionComponents_ListView.TabIndex = 1;
+            this.SolutionComponents_ListView.UseCompatibleStateImageBehavior = false;
+            this.SolutionComponents_ListView.View = System.Windows.Forms.View.List;
+            this.SolutionComponents_ListView.SelectedIndexChanged += new System.EventHandler(this.SolutionComponents_ListView_SelectedIndexChanged);
+            // 
+            // output_txt
+            // 
+            this.output_txt.AutoSize = true;
+            this.output_txt.Location = new System.Drawing.Point(24, 410);
+            this.output_txt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output_txt.Name = "output_txt";
+            this.output_txt.Size = new System.Drawing.Size(200, 90);
+            this.output_txt.TabIndex = 25;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.SolutionComponents_ListView);
+            this.panel1.Controls.Add(this.toolStrip2);
+            this.panel1.Location = new System.Drawing.Point(24, this.output_txt.Height + this.output_txt.Location.Y);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1940, 598);
+            this.panel1.MinimumSize = new System.Drawing.Size(1170, 0);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
+            // 
+            // OpenSolution_Computer
+            // 
+            this.OpenSolution_Computer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenSolution_Computer.Controls.Add(this.LoadSolution_Button);
+            this.OpenSolution_Computer.Location = new System.Drawing.Point(24, 222);
+            this.OpenSolution_Computer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OpenSolution_Computer.Name = "OpenSolution_Computer";
+            this.OpenSolution_Computer.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OpenSolution_Computer.Size = new System.Drawing.Size(1170, 110);
+            this.OpenSolution_Computer.MinimumSize = new System.Drawing.Size(1170, 110);
+            this.OpenSolution_Computer.TabIndex = 11;
+            this.OpenSolution_Computer.TabStop = false;
+            this.OpenSolution_Computer.Text = "Open a solution from your computer (Must be a .zip file exported from xRM)";
+            this.OpenSolution_Computer.Enter += new System.EventHandler(this.OpenSolution_Computer_Enter);
+            // 
+            // LoadSolution_Button
+            // 
+            this.LoadSolution_Button.Location = new System.Drawing.Point(8, 32);
+            this.LoadSolution_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LoadSolution_Button.Name = "LoadSolution_Button";
+            this.LoadSolution_Button.Size = new System.Drawing.Size(200, 58);
+            this.LoadSolution_Button.TabIndex = 1;
+            this.LoadSolution_Button.Text = "Open Solution...";
+            this.LoadSolution_Button.UseVisualStyleBackColor = true;
+            this.LoadSolution_Button.Click += new System.EventHandler(this.LoadSolution_Button_Click);
+            // 
+            // OpenSolution
+            // 
+            this.OpenSolution.FileName = "OpenSolution";
+            // 
+            // txt_OutputPath
+            // 
+            this.txt_OutputPath.Location = new System.Drawing.Point(134, 358);
+            this.txt_OutputPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_OutputPath.Name = "txt_OutputPath";
+            this.txt_OutputPath.Size = new System.Drawing.Size(1036, 31);
+            this.txt_OutputPath.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 362);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Output To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(870, 61);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "XRM Solution Dependency Checker";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1274, 107);
+            this.textBox1.TabIndex = 28;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // PluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output_txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_OutputPath);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.OpenSolution_Computer);
+            this.AutoScroll = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "PluginControl";
+            this.Size = new System.Drawing.Size(2084, 1081);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.OpenSolution_Computer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker BackgroundProgressIndicator;
+        private System.Windows.Forms.ToolStripLabel InfoLabel;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ImageList ComonentIcons;
+        private System.Windows.Forms.ListView SolutionComponents_ListView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox OpenSolution_Computer;
+        private System.Windows.Forms.Button LoadSolution_Button;
+        private System.Windows.Forms.OpenFileDialog OpenSolution;
+        private System.Windows.Forms.TextBox txt_OutputPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label output_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
